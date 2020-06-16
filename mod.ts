3931c2ca5ea0db5f9968112f032f8455a48e00b9
iFaxity <izze96@gmail.com>
2020-06-16 14:25:55 +0200

    refactor: added credits

--- mod.ts
+++ mod.ts
@@ -1,3 +1,10 @@
+/*!
+ * Based on https://github.com/jshttp/fresh/blob/master/index.js
+ * Copyright(c) 2012 TJ Holowaychuk
+ * Copyright(c) 2016-2017 Douglas Christopher Wilson
+ * Copyright(c) 2020 Douglas Christian Norrman
+ * MIT Licensed
+ */
 const CACHE_CONTROL_NO_CACHE_REGEXP = /(?:^|,)\s*?no-cache\s*?(?:,|$)/;
 const NONE_MATCH_REGEXP = /("| |W\/)/g;
 
@@ -11,17 +18,9 @@ export default function fresh(reqHeaders: Headers, resHeaders: Headers): boolean
     return false;
   }
 
-  // Always return stale when Cache-Control: no-cache
-  // to support end-to-end reload requests
-  // https://tools.ietf.org/html/rfc2616#section-14.9.4
-  const cacheControl = reqHeaders.get('Cache-Control');
-  if (cacheControl && CACHE_CONTROL_NO_CACHE_REGEXP.test(cacheControl)) {
-    return false;
-  }
-
   // If-None-Match
   if (noneMatch && noneMatch !== '*') {
-    const etag = resHeaders.get('ETag')?.replace(NONE_MATCH_REGEXP, '');
+    const etag = resHeaders.get('ETag');
     if (!etag) {
       return false;
     }
@@ -38,6 +37,14 @@ export default function fresh(reqHeaders: Headers, resHeaders: Headers): boolean
     if (!lastModified || Date.parse(lastModified) > Date.parse(modifiedSince)) {
       return false;
     }
+  }
+
+  // Always return stale when Cache-Control: no-cache
+  // to support end-to-end reload requests
+  // https://tools.ietf.org/html/rfc2616#section-14.9.4
+  const cacheControl = reqHeaders.get('Cache-Control');
+  if (cacheControl && CACHE_CONTROL_NO_CACHE_REGEXP.test(cacheControl)) {
+    return false;
   }
 
   return true;
